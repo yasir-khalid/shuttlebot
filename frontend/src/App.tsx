3ae25d6efd8c3495ae2c574b81839a62414403aa
muhammad-usama-aleem <usamaaleem2000@gmail.com>
2024-03-11 18:01:37 +0500

    formatting and frontend init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Route, Routes } from 'react-router';
+import Home from './components/Home';
+
+function App() {
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
